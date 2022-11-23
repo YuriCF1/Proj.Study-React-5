4a859eca0e98f0074b221349142743493153c6de
Yuri <yurifdev@gmail.com>
2022-11-22 23:49:34 -0300

    Creating the first form

---
+++ form/src/components/MyForm.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './MyForm.css'
+
+const MyForm = () => {
+  return (
+    <div>
+        {/* Criação de form */}
+        <form>
+            <div>
+                <label htmlFor="name">Nome</label>
+                <input type="text" name="name" placeholder='Digite seu nome'/>
+            </div>
+            <input type="submit" name="" id="" value="Enviar"/>
+        </form>
+    </div>
+  )
+}
+
+export default MyForm
