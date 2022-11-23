3da493aaf02d2444c5193b520af7ad690f92ce5e
Yuri <yurifdev@gmail.com>
2022-11-23 09:33:36 -0300

    Creating inputs with label

--- form/src/components/MyForm.js
+++ form/src/components/MyForm.js
@@ -4,12 +4,17 @@ import './MyForm.css'
 const MyForm = () => {
   return (
     <div>
-        {/* Criação de form */}
+        {/* Criação de form, de com o htmlFor */}
         <form>
             <div>
                 <label htmlFor="name">Nome</label>
                 <input type="text" name="name" placeholder='Digite seu nome'/>
             </div>
+            {/* Label envolvendo input, mais sugerida na documentação do react  */}
+            <label>
+              <span>Email</span>
+              <input type="email" name="email" placeholder='Digite o seu email'/>
+            </label>
             <input type="submit" name="" id="" value="Enviar"/>
         </form>
     </div>
