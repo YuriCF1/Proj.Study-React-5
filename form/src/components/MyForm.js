a0aafa8292f536f68b47c91ffa2a107e4fe9c9c0
Yuri <yurifdev@gmail.com>
2022-11-24 18:44:10 -0300

    Dealing with input's values

--- form/src/components/MyForm.js
+++ form/src/components/MyForm.js
@@ -1,24 +1,42 @@
-import React from 'react'
-import './MyForm.css'
+import React, { useState } from "react";
+import "./MyForm.css";
 
 const MyForm = () => {
+  // 3 - gerenciamento de dados
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+
+  const handleName = (e) => {
+    console.log("Mudou o nome");
+    console.log(e);
+    console.log(e.target.value);
+    setName(e.target.value);
+  };
+  
+  console.log('NOME', name);
+
   return (
     <div>
-        {/* Criação de form, de com o htmlFor */}
-        <form>
-            <div>
-                <label htmlFor="name">Nome</label>
-                <input type="text" name="name" placeholder='Digite seu nome'/>
-            </div>
-            {/* Label envolvendo input, mais sugerida na documentação do react  */}
-            <label>
-              <span>Email</span>
-              <input type="email" name="email" placeholder='Digite o seu email'/>
-            </label>
-            <input type="submit" name="" id="" value="Enviar"/>
-        </form>
+      {/* Criação de form, de com o htmlFor */}
+      <form>
+        <div>
+          <label htmlFor="name">Nome</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Digite seu nome"
+            onChange={handleName}
+          />
+        </div>
+        {/* Label envolvendo input, mais sugerida na documentação do react  */}
+        <label>
+          <span>Email</span>
+          <input type="email" name="email" placeholder="Digite o seu email" />
+        </label>
+        <input type="submit" name="" id="" value="Enviar" />
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default MyForm
+export default MyForm;
